6d5167b97ce21b0cb2b11d2044a60e1c2d8b54a3
Olek <olekczubiak@protonmail.com>
2021-09-02 15:59:22 +0200

    update main

--- simple-tracker/app/main.py
+++ simple-tracker/app/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
+app.simple_db = []
+
+
+class Position(BaseModel):
+    time: str
+    cords: str
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": f"Check POST METHOD: {app.simple_db}"}
+
+@app.post("/poz")
+async def send_poz(poz: Position):
+    print(f"Dodanie nowego rekordu {poz}")
+    app.simple_db.append(poz)
+    return poz
+
+@app.get("/list")
+def list_of_poz():
+    return app.simple_db
