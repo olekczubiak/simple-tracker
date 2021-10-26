8cdeac94582258186496a7204d9d1c8d4e468755
Olek <olekczubiak@protonmail.com>
2021-10-26 18:53:21 +0200

    checking if last time record exist in db

--- simple-tracker/app/crud.py
+++ simple-tracker/app/crud.py
@@ -1,8 +1,13 @@
 from sqlalchemy.orm import Session
 
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.sql.elements import True_
+from sqlalchemy.sql.expression import desc
 
 from . import models, schemas
+
+def get_device_info(db: Session, my_owner_id: int):
+    return db.query(models.Device).filter(models.Device.owner_id == my_owner_id).all()
 
 def get_position(db: Session):
     return db.query(models.Position).all()
@@ -23,3 +28,6 @@ def create_position(db: Session, item: schemas.PositionSchema):
 
 def check_user_id_exist(db: Session, my_owner_id: int):
     return db.query(models.User.id).filter(models.User.id == my_owner_id).first()
+
+def check_if_last_time_exist(db: Session):
+    return db.query(models.Position.time).order_by(models.Position.id.desc()).first()[0]
--- simple-tracker/app/routers.py
+++ simple-tracker/app/routers.py
@@ -6,12 +6,6 @@ from . import crud, schemas
 
 router = APIRouter()
 
-
-# @router.get("/")
-# async def root():
-#     return {"message": f"Go to /api to get api, go to /docs to see docs"}
-
-
 @router.get("/api/")
 async def api_info():
     return {"message": f"API for the live monitoring and analyzing. Go to /docs for more info. "}
@@ -20,16 +14,22 @@ async def api_info():
 async def get_live_poz(db: Session = Depends(get_db)):
     return crud.get_live_position(db)
 
-@router.get("/api/list/")
+@router.get("/api/list")
 async def get_listed_poz(db: Session = Depends(get_db)):
     return crud.get_position(db)
 
-@router.post("/api/add/",  response_model=schemas.PositionSchema)
+@router.post("/api/add",  response_model=schemas.PositionSchema)
 async def send_poz(item: schemas.PositionSchema, db: Session = Depends(get_db)):
     if not crud.check_user_id_exist(db, item.owner_id):
-        raise HTTPException(status_code=409, detail="No owner_id in db")
-    return crud.create_position(db=db, item=item)
+        raise HTTPException(status_code=404, detail="No owner_id found")
+    if crud.check_if_last_time_exist(db) == item.time:
+        raise HTTPException(status_code=409, detail="Conflict")
+    else:
+        return crud.create_position(db=db, item=item)
 
-# @router.get("/api/deive/")
-# async def device_info():
-#     return {"message": "Pobiera z bazy danych informacje o urządeniu"}
+@router.get("/api/device/{owner_id}")
+async def device_info(owner_id: int, db: Session = Depends(get_db)):
+    db_query = crud.get_device_info(db, owner_id)
+    if len(db_query) == 0:
+        raise HTTPException(status_code=404, detail="No owner_id found")
+    return db_query
