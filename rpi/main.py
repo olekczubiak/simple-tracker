3d1443dda943e049a935bcb0777695335499f621
Olek <olekczubiak@protonmail.com>
2021-10-28 17:35:32 +0200

    add auth to request, dont send if no speed

--- rpi/main.py
+++ rpi/main.py
@@ -7,7 +7,8 @@ import saver
 import requester
 
 TODAY_DATE = str(datetime.datetime.now())
-URL = "https://tracker.toadres.pl/add"
+URL = "https://tracker.toadres.pl/api/add"
+ACCESS_TOKEN = '48cdcb6f7300a42157e9a78db71b7cc103609b7584dcffc12ca20605110b8e67'
 
 def print_data(date, lat, lng, speed):
 	print("Date=\x1b[2;34;40m" + date + "\x1b[0m and Latitude=\x1b[3;31;40m" + str(lat) + "\x1b[0m and Longitude=\x1b[3;31;40m" + str(lng) + "\x1b[0m Speed=\x1b[1;33;40m" + str(speed) + "\x1b[0m")
@@ -25,6 +26,9 @@ while True:
 		date = str(datetime.datetime.now())
 
 		saver.save_to_file(TODAY_DATE[:10], saver.data_to_save(date[11:19], lat, lng))
-		requester.send_data(date[11:19], lat, lng)
+		if speed == None:
+			pass
+		else:
+			requester.send_data(URL, ACCESS_TOKEN, TODAY_DATE[:10], date[11:19], lat, lng)
 		print "\n"
 		print_data(date, lat, lng, speed)
--- rpi/requester.py
+++ rpi/requester.py
@@ -1,4 +1,5 @@
 import os
-def send_data(data, lat, lng):
-    data = "curl -X \'POST\'   \'https://tracker.toadres.pl/add\'   -H \'accept: application/json\'   -H \'Content-Type: application/json\'   -d \'{\"time\": \"%s\", \"longitude\": \"%s\", \"latitude\": \"%s\"}\'" %(data, lng, lat)
+
+def send_data(url, access_token, data, time, lat, lng):
+    data = 'curl -X \'POST\' \'%s\' -H \'accept: application/json\'  -H \'Authorization: Bearer %s\' -H \'Content-Type: application/json\' -d \'{\"owner_id\": 0, \"my_date\": \"%s\",\"time\": \"%s\", \"longitude\": %s, \"latitude\": %s}\'' %(url, access_token, data, time, lng, lat)
     return os.system(data)
