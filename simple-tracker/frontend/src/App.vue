b50090134a83b1717e6bafccfa0b2b68e9c0e591
Olek <olekczubiak@protonmail.com>
2021-10-29 20:28:49 +0200

    new app with router

--- simple-tracker/frontend/src/App.vue
+++ simple-tracker/frontend/src/App.vue
@@ -1,39 +1,32 @@
 <template>
   <div id="app">
-    <Claim/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Claim from '@/components/Claim.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Claim
-  }
-}
-</script>
-
 <style>
-@import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,600,800');
-* {
-  box-sizing: border-box;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-body {
-  font-family: 'Montserrat', sans-serif;
-  margin: 0;
-  padding: 0;
-  margin: 0;
-  position: relative;
-  width: 100%;
-  min-height: 100vh;
+
+#nav {
   padding: 30px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
