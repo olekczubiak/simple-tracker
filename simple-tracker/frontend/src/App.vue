623b461a10eb5718e3fa80644c760487a9fe3979
Olek <olekczubiak@protonmail.com>
2021-10-29 17:05:59 +0200

    make main page

--- simple-tracker/frontend/src/App.vue
+++ simple-tracker/frontend/src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Claim/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Claim from '@/components/Claim.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Claim
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,600,800');
+* {
+  box-sizing: border-box;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  font-family: 'Montserrat', sans-serif;
+  margin: 0;
+  padding: 0;
+  margin: 0;
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
