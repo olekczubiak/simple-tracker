a3d7e9efbc5aef9171ea559dd5ba30936c1334bb
Olek <olekczubiak@protonmail.com>
2021-11-05 16:59:18 +0100

    make vievs for the panel

---
+++ simple-tracker/frontend/src/views/Monitor.vue
@@ -0,0 +1,39 @@
+<template>
+    <Nav/>
+        <div class="row">
+    <SlideBar/>
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Monitorowanie:</h1>
+
+
+    <!-- Default switch -->
+
+        <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+            <label class="form-check-label" for="flexSwitchCheckDefault">Automatyczne odswiezanie rekordow</label>
+        </div>
+
+        </div>
+
+        <GetLivePoz/>
+        </main>
+    </div>
+</template>
+
+<script>
+import Nav from "@/components/Nav.vue"
+import SlideBar from "@/components/SlideBar.vue"
+import GetLivePoz from "@/components/GetLivePoz.vue"
+
+
+export default {
+    name: 'Panel',
+    components: {
+        Nav,
+        SlideBar,
+        GetLivePoz,
+    },
+}
+</script>
--- simple-tracker/frontend/src/views/Panel.vue
+++ simple-tracker/frontend/src/views/Panel.vue
@@ -5,16 +5,7 @@
 
   <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Urzadzenia:</h1>
-
-    
-  <!-- Default switch -->
-  
-    <div class="form-check form-switch">
-      <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
-      <label class="form-check-label" for="flexSwitchCheckDefault">Automatyczne odswiezanie rekordow</label>
-    </div>
-
+    <h1 class="h2">Storna panelu:</h1>
   </div>
   
   <GetLivePoz/>
@@ -25,15 +16,13 @@
 <script>
 import Nav from "@/components/Nav.vue"
 import SlideBar from "@/components/SlideBar.vue"
-import GetLivePoz from "@/components/GetLivePoz.vue"
 
 
 export default {
     name: 'Panel',
     components: {
       Nav,
-      SlideBar,
-      GetLivePoz,
+      SlideBar
     },
   }
 </script>
